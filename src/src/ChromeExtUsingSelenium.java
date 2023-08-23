c0fba2d95fb28838e08e9a69c753bbce1520c4fe
Tatjana Sadovska <tatjana.sadovska@testdevlab.com>
2023-08-23 19:31:06 +0300

    connect 'From' wallet

--- src/src/ChromeExtUsingSelenium.java
+++ src/src/ChromeExtUsingSelenium.java
@@ -1,5 +1,6 @@
 package src;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -10,6 +11,31 @@ public class ChromeExtUsingSelenium {
         opt.addArguments("profile-directory=Default");
         ChromeDriver driver = new ChromeDriver(opt);
 
-        driver.get("https://google.com/");
+        driver.get("https://wormhole-connect.netlify.app/");
+
+        synchronized (driver) {
+            driver.wait(2000);
+        }
+
+        driver.findElement(By.xpath("//*[text()='Connect wallet']")).click();
+
+        synchronized (driver) {
+            driver.wait(3000);
+        }
+
+        String windowMain = (String) driver.getWindowHandles().toArray()[0];
+
+        driver.findElement(By.xpath("//*[text()='Metamask']")).click();
+
+        synchronized (driver) {
+            driver.wait(3000);
+        }
+
+        String windowMetamask = (String) driver.getWindowHandles().toArray()[1];
+
+        driver.switchTo().window(windowMetamask);
+
+        System.out.println("we are at:");
+        System.out.println(driver.getCurrentUrl());
     }
 }
