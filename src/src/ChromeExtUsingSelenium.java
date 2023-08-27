adb5db2abcc2c6cafc16786c44294fefe93d5c9e
Tatjana Sadovska <tatjana.sadovska@testdevlab.com>
2023-08-27 19:32:02 +0300

    completed step 1 form

--- src/src/ChromeExtUsingSelenium.java
+++ src/src/ChromeExtUsingSelenium.java
@@ -1,41 +1,73 @@
 package src;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
 
 public class ChromeExtUsingSelenium {
     public static void main(String[] args) throws InterruptedException {
         ChromeOptions opt = new ChromeOptions();
-        opt.addArguments("user-data-dir=/Users/tatjana.sadovska/Library/Application Support/Google/Chrome");
+        opt.setBinary("/Applications/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing");
+        opt.addArguments("user-data-dir=/Users/tatjana.sadovska/Library/Application Support/Chromium");
         opt.addArguments("profile-directory=Default");
+        opt.addExtensions (new File("metamask-chrome-10.34.5.crx"));
         ChromeDriver driver = new ChromeDriver(opt);
 
         driver.get("https://wormhole-connect.netlify.app/");
 
-        synchronized (driver) {
-            driver.wait(2000);
-        }
+        Thread.sleep(1000);
 
-        driver.findElement(By.xpath("//*[text()='Connect wallet']")).click();
+        WebElement element = driver.findElement(By.xpath("//*[text()='Connect wallet']"));
+        element.click();
 
-        synchronized (driver) {
-            driver.wait(3000);
-        }
-
-        String windowMain = (String) driver.getWindowHandles().toArray()[0];
+        Thread.sleep(1000);
 
         driver.findElement(By.xpath("//*[text()='Metamask']")).click();
 
-        synchronized (driver) {
-            driver.wait(3000);
-        }
+        Thread.sleep(3000);
 
-        String windowMetamask = (String) driver.getWindowHandles().toArray()[1];
+        driver.switchTo().window((String) driver.getWindowHandles().toArray()[1]);
 
-        driver.switchTo().window(windowMetamask);
+        System.out.println("URL: " + driver.getCurrentUrl());
 
-        System.out.println("we are at:");
-        System.out.println(driver.getCurrentUrl());
+        driver.findElement(By.cssSelector("[data-testid='unlock-password']")).sendKeys(System.getenv("METAMASK_PASSWORD"));
+        driver.findElement(By.cssSelector("[data-testid='unlock-submit']")).click();
+
+        Thread.sleep(1000);
+
+        driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
+
+        driver.findElement(By.xpath("//*[text()='Connect wallet']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[text()='Metamask']")).click();
+        Thread.sleep(500);
+
+        driver.findElement(By.xpath("//*[text()='Select network']")).click();
+        Thread.sleep(500);
+
+        driver.findElement(By.xpath("//*[text()='Alfajores']")).click();
+        Thread.sleep(500);
+
+        driver.findElement(By.xpath("//*[text()='Select network']")).click();
+        Thread.sleep(500);
+
+        driver.findElement(By.xpath("//*[text()='Fantom']")).click();
+        Thread.sleep(500);
+
+        driver.findElement(By.xpath("//*[text()='Select']")).click();
+        Thread.sleep(500);
+
+        driver.findElement(By.xpath("//*[text()='CELO']")).findElement(By.xpath("../../..")).click();
+        Thread.sleep(500);
+
+        driver.findElement(By.id("sendAmt")).sendKeys("0.1");
+        Thread.sleep(2000);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 350)", "");
     }
 }
