8f8fec6b1acb2158230599c790ab2ddf90b4ac78
Tatjana Sadovska <tatjana.sadovska@testdevlab.com>
2024-02-20 13:11:31 +0200

    updated all tests suite

--- src/test/java/RunAllTests.java
+++ src/test/java/RunAllTests.java
@@ -3,6 +3,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/testnet/", tags = "not @ignore")
+@CucumberOptions(features = "src/test/resources/testnet/", tags = "not @ignore and not @new")
 public class RunAllTests {
 }
